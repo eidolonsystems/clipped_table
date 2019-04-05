e71043bcb07e04a4aed87d857e6117a27990837e
Sofiya Polyanska <sofiya@spiretrading.com>
2019-04-05 17:49:09 -0400

    Added tests and most of the fucntionality.

--- source/translated_table_model.ts
+++ source/translated_table_model.ts
@@ -1,5 +1,6 @@
 import * as Kola from 'kola-signals';
-import { Operation } from './operations';
+import { Operation, AddRowOperation, MoveRowOperation, 
+  RemoveRowOperation, UpdateValueOperation } from './operations';
 import { TableModel } from './table_model';
 
 /** Adapts an existing TableModel with the ability to rearrange rows. */
@@ -10,16 +11,33 @@ export class TranslatedTableModel extends TableModel {
    */
   constructor(model: TableModel) {
     super();
+    this.model = model;
+    this.references = new Array(model.rowCount);
+    this.transactionCount = 0;
+    this.dispatcher = new Kola.Dispatcher<Operation[]>();
+    model.connect(
+      (operations: Operation[]) => {
+      this.handleOperations(operations);});
   }
 
   /** Marks the beginning of a transaction. In cases where a transaction is
    *  already being processed, then the sub-transaction gets consolidated into
    *  the parent transaction.
    */
-  public beginTransaction(): void {}
+  public beginTransaction(): void {
+    if(this.transactionCount === 0) {
+      this.operations = [];
+    }
+    ++this.transactionCount;
+  }
 
   /** Ends a transaction. */
-  public endTransaction(): void {}
+  public endTransaction(): void {
+    --this.transactionCount;
+    if(this.transactionCount === 0) {
+      this.dispatcher.dispatch(this.operations);
+    }
+  }
 
   /** Moves a row.
    * @param source - The index of the row to move.
@@ -28,22 +46,114 @@ export class TranslatedTableModel extends TableModel {
    *                      to this table's columnCount.
    * @throws RangeError - The index specified is not within range.
    */
-  public moveRow(source: number, destination: number): void {}
+  public moveRow(source: number, destination: number): void {
+    if(source > this.rowCount || source < 0) {
+      throw RangeError();
+    }
+    if(destination > this.rowCount || destination < 0) {
+      throw RangeError();
+    }
+    this.beginTransaction();
+    let traveller;
+    if(this.references[source] >= 0) {
+      traveller = this.references[source];
+    } else {
+      traveller = source;
+    }
+    if(source > destination) {
+      this.moveDown(source, destination);
+    } else if(destination > source) {
+      this.moveUp(source, destination);
+    }
+    this.references[destination] = traveller;
+    this.operations.push(new MoveRowOperation(source, destination));
+    this.endTransaction();
+  }
 
   public get rowCount(): number {
-    return 0;
+    return this.model.rowCount.valueOf();
   }
 
   public get columnCount(): number {
-    return 0;
+    return this.model.columnCount.valueOf();
   }
 
   public get(row: number, column: number): any {
-    return null;
+    if(row >= this.rowCount || row  < 0) {
+      throw RangeError();
+    }
+    if(column >= this.columnCount  || column < 0) {
+      throw RangeError();
+    }
+    if(this.references[row] >= 0) {
+        return this.model.get(this.references[row], column);
+    } else {
+      return this.model.get(row, column);
+    }
   }
 
   public connect(slot: (operations: Operation[]) => void):
       Kola.Listener<Operation[]> {
-    return null;
+    return this.dispatcher.listen(slot);
   }
+
+  private handleOperations(newOperations: Operation[]): void {
+    for(const operation of newOperations) {
+      if(operation instanceof AddRowOperation) {
+        this.references.push();
+        this.moveDown(operation.index, this.references.length);
+        this.references[operation.index] = operation.index;
+      } else if (operation instanceof MoveRowOperation) {
+        this.moveRow(operation.source, operation.destination);
+      } else if (operation instanceof RemoveRowOperation) {
+        this.rowRemoved(operation.index);
+      } else if (operation instanceof UpdateValueOperation) {
+        // do nothing
+      } else {
+        throw TypeError;
+      }
+    }
+  }
+
+  private moveUp(start: number, end: number) {
+    for(let index = start; index < end; index++ ) {
+      if(this.references[index + 1] >= 0) {
+        this.references[index] = this.references[index + 1];
+      } else {
+        this.references[index] = index + 1;
+      }
+    }
+  }
+
+  private moveDown(start: number, end: number) {
+    for(let index = start; index > end ; --index ) {
+      if(this.references[index - 1] >= 0) {
+        this.references[index] = this.references[index - 1];
+      } else {
+        this.references[index] = index - 1;
+      }
+    }
+  }
+
+  private rowRemoved(deletedRow: number) {
+    let referenceIndex = -1;
+    for(let index = 0; index < this.references.length; index++ ) {
+      if(this.references[index] === deletedRow) {
+        referenceIndex = index;
+      }
+    }
+    this.moveUp(referenceIndex, this.references.length);
+    this.references.pop();
+    for(let index = 0; index < this.references.length; index++ ) {
+      if(this.references[index] > deletedRow) {
+        this.references[index] = this.references[index] - 1;
+      }
+    }
+  }
+
+  private model: TableModel;
+  private references: any[];
+  private transactionCount: number;
+  private operations: Operation[];
+  private dispatcher: Kola.Dispatcher<Operation[]>;
 }
--- tests/translated_table_model_tester.ts
+++ tests/translated_table_model_tester.ts
@@ -1,5 +1,167 @@
 import { Expect, Test } from 'alsatian';
+import { ArrayTableModel, MoveRowOperation,  Operation,
+  TranslatedTableModel } from '../source';
 
 /** Tests the TranslatedTableModel. */
 export class TranslatedTableModelTester {
+
+  /** Tests if the number of rows is correct */
+  @Test()
+  public testRowCount(): void {
+    const model = new ArrayTableModel();
+    const translatedTable = new TranslatedTableModel(model);
+    Expect(translatedTable.rowCount).toEqual(0);
+    model.addRow([1, 2 ,3]);
+    model.addRow([4, 5, 6]);
+    model.addRow([7, 8, 9]);
+    const translatedTable2 = new TranslatedTableModel(model);
+    Expect(translatedTable2.rowCount).toEqual(3);
+  }
+
+  /** Tests if the number of columns is corrrect. */
+  @Test()
+  public testColumnCount(): void {
+    const model = new ArrayTableModel();
+    const translatedTable = new TranslatedTableModel(model);
+    Expect(translatedTable.columnCount).toEqual(0);
+    model.addRow([1, 2, 3, 0]);
+    model.addRow([4, 5, 6, 0]);
+    model.addRow([7, 8, 9, 0]);
+    const translatedTable2 = new TranslatedTableModel(model);
+    Expect(translatedTable2.columnCount).toEqual(4);
+  }
+
+  /** Tests if the get function works. */
+  @Test()
+  public testGet(): void {
+    const model = new ArrayTableModel();
+    const translatedTable = new TranslatedTableModel(model);
+    model.addRow([1, 2, 3, 4, 5]);
+    model.addRow([6, 7, 8, 9, 10]);
+    Expect(translatedTable.get(0,1)).toEqual(2);
+    Expect(translatedTable.get(1,3)).toEqual(9);
+  }
+
+  /** Tests the move fucntion. */
+  @Test()
+  public testMoveRow(): void {
+    const model = new ArrayTableModel();
+    model.addRow([1, 2, 1, 2]);
+    model.addRow([3, 4, 3, 4]);
+    model.addRow([5, 6, 5, 6]);
+    model.addRow([7, 8, 7, 8]);
+    model.addRow([9, 0, 9, 0]);
+    const translatedTable = new TranslatedTableModel(model);
+    Expect(() => translatedTable.moveRow(-1, 3)).toThrow();
+    Expect(() => translatedTable.moveRow(10, 3)).toThrow();
+    translatedTable.moveRow(0, 2);
+    Expect(translatedTable.get(0,0)).toEqual(3);
+    Expect(translatedTable.get(1,0)).toEqual(5);
+    Expect(translatedTable.get(2,0)).toEqual(1);
+    Expect(translatedTable.get(3,0)).toEqual(7);
+    Expect(translatedTable.get(4,0)).toEqual(9);
+    translatedTable.moveRow(3, 1);
+    Expect(translatedTable.get(0,0)).toEqual(3);
+    Expect(translatedTable.get(1,0)).toEqual(7);
+    Expect(translatedTable.get(2,0)).toEqual(5);
+    Expect(translatedTable.get(3,0)).toEqual(1);
+    Expect(translatedTable.get(4,0)).toEqual(9);
+    translatedTable.moveRow(3, 3);
+    Expect(translatedTable.get(0,0)).toEqual(3);
+    Expect(translatedTable.get(1,0)).toEqual(7);
+    Expect(translatedTable.get(2,0)).toEqual(5);
+    Expect(translatedTable.get(3,0)).toEqual(1);
+    Expect(translatedTable.get(4,0)).toEqual(9);
+    translatedTable.moveRow(0, 4);
+    Expect(translatedTable.get(0,0)).toEqual(7);
+    Expect(translatedTable.get(1,0)).toEqual(5);
+    Expect(translatedTable.get(2,0)).toEqual(1);
+    Expect(translatedTable.get(3,0)).toEqual(9);
+    Expect(translatedTable.get(4,0)).toEqual(3);
+    translatedTable.moveRow(4, 0);
+    Expect(translatedTable.get(0,0)).toEqual(3);
+    Expect(translatedTable.get(1,0)).toEqual(7);
+    Expect(translatedTable.get(2,0)).toEqual(5);
+    Expect(translatedTable.get(3,0)).toEqual(1);
+    Expect(translatedTable.get(4,0)).toEqual(9);
+  }
+
+  /** Tests if signaling for the move operation works. */
+  @Test()
+  public testTwoMoves(): void {
+    const model = new ArrayTableModel();
+    model.addRow([1, 2]);
+    model.addRow([3, 4]);
+    model.addRow([5, 6]);
+    model.addRow([7, 8]);
+    model.addRow([9, 0]);
+    const translatedTable = new TranslatedTableModel(model);
+    let numberOfOperations = 0;
+    const slot = (operations: Operation[]) => {
+      if(operations) {
+        console.log(operations);
+        numberOfOperations = operations.length;
+      } else {
+        Expect(false).toEqual(true);
+      }
+    };
+    const listener = translatedTable.connect(slot);
+    translatedTable.moveRow(0, 2);
+    translatedTable.moveRow(1, 3);
+    Expect(numberOfOperations).toEqual(1);
+    listener.unlisten();
+  }
+
+  /** Tests when a row is added to the original model. */
+  @Test()
+  public testSignalListeningAdd(): void {
+    const model = new ArrayTableModel();
+    model.addRow([1, 2]);
+    model.addRow([3, 4]);
+    model.addRow([5, 6]);
+    const translatedTable = new TranslatedTableModel(model);
+    model.addRow([0, 0], 1);
+    Expect(translatedTable.get(0, 0)).toEqual(1);
+    Expect(translatedTable.get(1, 0)).toEqual(0);
+    Expect(translatedTable.get(2, 0)).toEqual(3);
+    Expect(translatedTable.get(3, 0)).toEqual(5);
+  }
+
+  /** Tests when a row is removed from the original model. */
+  @Test()
+  public testSignalListeningRemove(): void {
+    const model = new ArrayTableModel();
+    model.addRow([1, 2]);
+    model.addRow([3, 4]);
+    model.addRow([5, 6]);
+    model.addRow([7, 8]);
+    const translatedTable = new TranslatedTableModel(model);
+    translatedTable.moveRow(2, 0);
+    model.removeRow(1);
+    console.log(model);
+    console.log(translatedTable);
+    model.get(0,0);
+    translatedTable.get(0, 0);
+    Expect(translatedTable.get(0, 0)).toEqual(5);
+    Expect(translatedTable.get(1, 0)).toEqual(1);
+    Expect(translatedTable.get(2, 0)).toEqual(7);
+  }
+
+  /** Tests when a cell is updated in the original model. */
+  @Test()
+  public testUpdatedValues(): void {
+    const model = new ArrayTableModel();
+    model.addRow([0, 0]);
+    model.addRow([1, 1]);
+    model.addRow([2, 2]);
+    model.addRow([3, 3]);
+    const translatedTable = new TranslatedTableModel(model);
+    translatedTable.moveRow(2, 0);
+    model.set(0, 0, 9);
+    model.set(0, 1, 9);
+    Expect(translatedTable.get(0, 0)).toEqual(2);
+    Expect(translatedTable.get(0, 1)).toEqual(2);
+    Expect(translatedTable.get(1, 0)).toEqual(9);
+    Expect(translatedTable.get(1, 1)).toEqual(9);
+  }
 }
