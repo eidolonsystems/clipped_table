8c8fbb138f783771d445e33739b71f38522cd08b
Sofiya Polyanska <sofiya@spiretrading.com>
2019-04-18 13:00:49 -0400

    Improved the tests.

--- tests/comparator_tester.ts
+++ tests/comparator_tester.ts
@@ -19,9 +19,9 @@ export class ComparatorTestor {
     const comparator = new Comparator();
     Expect(comparator.compareValues(true, true)).toEqual(0);
     Expect(comparator.compareValues(false, false)).toEqual(0);
-    Expect(comparator.compareValues(false, true)).toEqual(-1);
-    Expect(comparator.compareValues(true, false)).toEqual(1);
-    Expect(comparator.compareValues(null, false)).toEqual(-1);
+    Expect(comparator.compareValues(false, true)).toBeLessThan(0);
+    Expect(comparator.compareValues(true, false)).toBeGreaterThan(0);
+    Expect(comparator.compareValues(null, false)).toBeLessThan(0);
   }
 
   /** Tests comparing numbers. */
@@ -29,12 +29,12 @@ export class ComparatorTestor {
   public compareNumbers(): void {
     const comparator = new Comparator();
     Expect(comparator.compareValues(-50000.989089, -50000.989089)).toEqual(0);
-    Expect(comparator.compareValues(50000, 5.5)).toEqual(1);
-    Expect(comparator.compareValues(0.03, 0.0000095)).toEqual(1);
-    Expect(comparator.compareValues(60, -1435)).toEqual(1);
-    Expect(comparator.compareValues(Infinity, 0)).toEqual(1);
-    Expect(comparator.compareValues(-Infinity, 0)).toEqual(-1);
-    Expect(comparator.compareValues(Infinity, NaN)).toEqual(1);
+    Expect(comparator.compareValues(50000, 5.5)).toBeGreaterThan(0);
+    Expect(comparator.compareValues(0.00003, 0.03)).toBeLessThan(0);
+    Expect(comparator.compareValues(60, -1435)).toBeGreaterThan(0);
+    Expect(comparator.compareValues(Infinity, 0)).toBeGreaterThan(0);
+    Expect(comparator.compareValues(-Infinity, 0)).toBeLessThan(0);
+    Expect(comparator.compareValues(Infinity, NaN)).toBeGreaterThan(0);
   }
 
   /* Tests comparing dates. */
@@ -44,9 +44,9 @@ export class ComparatorTestor {
     const laterDate = new Date(2005, 10, 2);
     const comparator = new Comparator();
     Expect(comparator.compareValues(earlierDate, earlierDate)).toEqual(0);
-    Expect(comparator.compareValues(earlierDate, laterDate)).toEqual(-1);
-    Expect(comparator.compareValues(laterDate, earlierDate)).toEqual(1);
-    Expect(comparator.compareValues(laterDate, null)).toEqual(1);
+    Expect(comparator.compareValues(earlierDate, laterDate)).toBeLessThan(0);
+    Expect(comparator.compareValues(laterDate, earlierDate)).toBeGreaterThan(0);
+    Expect(comparator.compareValues(laterDate, null)).toBeGreaterThan(0);
   }
 
   /** Tests comparing strings. */
@@ -54,8 +54,8 @@ export class ComparatorTestor {
   public compareStrings(): void {
     const comparator = new Comparator();
     Expect(comparator.compareValues('Spire', 'Spire')).toEqual(0);
-    Expect(comparator.compareValues('Spire', 'spire')).toEqual(1);
-    Expect(comparator.compareValues('réservé', 'reserve')).toEqual(1);
+    Expect(comparator.compareValues('Spire', 'spire')).toBeGreaterThan(0);
+    Expect(comparator.compareValues('réservé', 'reserve')).toBeGreaterThan(0);
   }
 
   /** Tests comparing symbols. */
@@ -66,7 +66,7 @@ export class ComparatorTestor {
     const symbol2 = Symbol('42');
     const symbol3 = Symbol('spire');
     Expect(comparator.compareValues(symbol1, symbol1)).toEqual(0);
-    Expect(comparator.compareValues(symbol1, symbol2)).toEqual(1);
+    Expect(comparator.compareValues(symbol1, symbol2)).toBeGreaterThan(0);
     Expect(comparator.compareValues(symbol1, symbol3)).toEqual(0);
   }
 }
