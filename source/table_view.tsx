f8f1a1da2e76c758819e6baff47ecccd0c81243d
Sofiya Polyanska <sofiya@spiretrading.com>
2019-05-28 12:40:48 -0400

    Fixed spacing.

--- source/table_view.tsx
+++ source/table_view.tsx
@@ -16,7 +16,7 @@ export class TableView extends React.Component<Properties> {
       const row = [];
       for(let j = 0; j < this.props.model.columnCount; ++j) {
         row.push(
-          <td key={(i*this.props.model.columnCount)+j}>
+          <td key={(i * this.props.model.columnCount) + j}>
             {this.props.model.get(i, j)}
           </td>);
       }
