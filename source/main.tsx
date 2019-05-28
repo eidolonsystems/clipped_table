4dea4039f77fff8e0df655ad7adae2ed6121f872
Sofiya Polyanska <sofiya@spiretrading.com>
2019-05-28 12:03:52 -0400

    Undid changes to the test.

--- source/main.tsx
+++ source/main.tsx
@@ -1,4 +1,3 @@
-
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { ArrayTableModel } from './array_table_model';
@@ -7,7 +6,7 @@ import { TableView } from './table_view';
 const model = new ArrayTableModel();
 for(let row = 0; row < 100000; ++row) {
   const r = [];
-  for(let column = 0; model.columnCount < 1; ++column) {
+  for(let column = 0; column < model.columnCount; ++column) {
     r.push(Math.floor(Math.random() * 1000000));
   }
   model.addRow(r);
