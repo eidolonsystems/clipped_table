5c5238f2c1ede41207788bdf4677b4b2ba0aca8f
Sofiya Polyanska <sofiya@spiretrading.com>
2019-05-28 16:16:56 -0400

    Added a basic header.

--- source/main.tsx
+++ source/main.tsx
@@ -4,13 +4,15 @@ import { ArrayTableModel } from './array_table_model';
 import { TableView } from './table_view';
 
 const model = new ArrayTableModel();
-for(let row = 0; row < 100000; ++row) {
+for(let row = 0; row < 500; ++row) {
   const r = [];
-  for(let column = 0; column < 10; ++column) {
+  for(let column = 0; column < 3; ++column) {
     r.push(Math.floor(Math.random() * 1000000));
   }
   model.addRow(r);
 }
 
-ReactDOM.render(<TableView model={model}/>,
+const header = ['one', 'two', 'three'];
+
+ReactDOM.render(<TableView model={model} header={header}/>,
   document.getElementById('main'));
--- source/table_view.tsx
+++ source/table_view.tsx
@@ -6,11 +6,24 @@ interface Properties {
 
   /** The model to display. */
   model: TableModel;
+
+  /**  */
+  header: Array<string>
+
+  /** Specifies the CSS class of the BoxLayout HTML element. */
+  className?: string;
+
+  /** The CSS style to apply. */
+  style?: any;
 }
 
 /** Renders a TableModel to HTML. */
 export class TableView extends React.Component<Properties> {
   public render(): JSX.Element {
+    const header = [];
+    for(let i = 0; i < this.props.header.length; ++i) {
+      header.push(<td>{this.props.header[i]}</td>);
+    }
     const tableRows = [];
     for(let i = 0; i < this.props.model.rowCount; ++i) {
       const row = [];
@@ -24,6 +37,11 @@ export class TableView extends React.Component<Properties> {
     }
     return(
       <table>
+        <thead>
+          <tr>
+            {header}
+          </tr>
+        </thead>
         <tbody>
           {tableRows}
         </tbody>
