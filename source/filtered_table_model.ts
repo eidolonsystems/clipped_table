17f53d9245cff4ba7e927f3e83565daf546a7cfe
Sofiya Polyanska <sofiya@spiretrading.com>
2019-07-16 18:47:17 -0400

    Filled out model and test.

--- source/filtered_table_model.ts
+++ source/filtered_table_model.ts
@@ -2,16 +2,48 @@ import * as Kola from 'kola-signals';
 import { Operation } from './operations';
 import { TableModel } from './table_model';
 
+export abstract class Predicate {
+
+  /**
+   * @param  row - The row to apply the predicate to.
+   */
+  public abstract applyPredicate(row: any[]): boolean;
+}
+
 export class FilteredTableModel extends TableModel {
 
   /** Constructs a FilteredTableModel.
    * @param source - The underlying model to filter.
    */
-  public constructor(model: TableModel) {
+  public constructor(model: TableModel, predicate: Predicate) {
     super();
     this.model = model;
+    this.predicate = predicate;
     this.length = 0;
     this.filter();
+    this.transactionCount = 0;
+    this.operations = [];
+    this.dispatcher = new Kola.Dispatcher<Operation[]>();
+    this.model.connect(this.handleOperations.bind(this));
+  }
+
+  /** Marks the beginning of a transaction. In cases where a transaction is
+   *  already being processed, then the sub-transaction gets consolidated into
+   *  the parent transaction.
+   */
+  public beginTransaction(): void {
+    if(this.transactionCount === 0) {
+      this.operations = [];
+    }
+    ++this.transactionCount;
+  }
+
+  /** Ends a transaction. */
+  public endTransaction(): void {
+    --this.transactionCount;
+    if(this.transactionCount === 0) {
+      this.dispatcher.dispatch(this.operations);
+    }
   }
 
   public get rowCount(): number {
@@ -23,13 +55,24 @@ export class FilteredTableModel extends TableModel {
   }
 
   public get(row: number, column: number) {
-    throw new Error("Method not implemented.");
+    if(row >= this.length || row < 0) {
+      throw RangeError();
+    }
+    if(column >= this.columnCount || column < 0) {
+      throw RangeError();
+    }
   }
   public connect(slot: (operations: Operation[]) => void):
     Kola.Listener<Operation[]> {
     throw new Error("Method not implemented.");
   }
 
+  private handleOperations(newOperations: Operation[]): void {
+    this.beginTransaction();
+    for(const operation of newOperations) {
+    }
+    this.endTransaction();
+  }
   private filter() {
     this.visiblity = [];
     for(let i = 0; i < this.model.rowCount; ++i) {
@@ -42,9 +85,11 @@ export class FilteredTableModel extends TableModel {
     }
   }
 
-
-
   private model: TableModel;
   private visiblity: boolean[];
+  private predicate: Predicate;
   private length: number;
+  private transactionCount: number;
+  private operations: Operation[];
+  private dispatcher: Kola.Dispatcher<Operation[]>;
 }
--- tests/filtered_table_model_tester.ts
+++ tests/filtered_table_model_tester.ts
@@ -1,6 +1,17 @@
 import { Expect, Test } from 'alsatian';
 import { ArrayTableModel }
   from '../source';
+import { Predicate, FilteredTableModel } from '../source/filtered_table_model';
+
+class MockPredicate implements Predicate {
+  public applyPredicate(row: any[]): boolean {
+    if(row[0] > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
 /** Tests the FilteredTableModel. */
 export class FilteredTableModelTester {
@@ -8,5 +19,12 @@ export class FilteredTableModelTester {
   /** Tests if the number of rows is correct */
   @Test()
   public testRowCount(): void {
+    const model = new ArrayTableModel();
+    model.addRow([-6]);
+    model.addRow([1]);
+    model.addRow([-6]);
+    model.addRow([6]);
+    const sortedTable = new FilteredTableModel(model, new MockPredicate());
+    Expect(sortedTable.rowCount).toEqual(2);
   }
 }
