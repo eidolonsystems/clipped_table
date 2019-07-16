14f1e33f7965f52e914d4bca749efa97018b6a2e
Sofiya Polyanska <sofiya@spiretrading.com>
2019-07-16 17:25:58 -0400

    Added a empty tester for filtered table.

---
+++ tests/filtered_table_model_tester.ts
@@ -0,0 +1,12 @@
+import { Expect, Test } from 'alsatian';
+import { ArrayTableModel }
+  from '../source';
+
+/** Tests the FilteredTableModel. */
+export class FilteredTableModelTester {
+
+  /** Tests if the number of rows is correct */
+  @Test()
+  public testRowCount(): void {
+  }
+}
